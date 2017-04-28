7e7da16ed96324f0cc09702c1f85438b2392da7f
Kevin .M. Gitonga <kevinjones4199@gmail.com>
2017-04-28 12:53:02 +0300

    Update Config.java

--- app/src/main/java/com/medussasoft/betintipspro/config/Config.java
+++ app/src/main/java/com/medussasoft/betintipspro/config/Config.java
@@ -6,6 +6,6 @@ package com.medussasoft.betintipspro.config;
 public class Config {
     public static String Backendless_app_id="450E4D0A-AE3C-0999-FFD4-1FB99D9FB600";
     public  static  String Backendless_secret_key="C59DB734-7D87-7B0E-FFB0-611613A02000";
-    public  static String gcm_id="419664563598";
+    public  static String gcm_id="***6645635**";
     public static  String RestUrl ="http://ipandasoft.co.ke/betintips/api";
 }
